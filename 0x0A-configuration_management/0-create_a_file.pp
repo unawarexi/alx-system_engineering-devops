8be23b2868623e9cea926a71af0914676f36ef57
unawarexi <unawarexi@gmail.com>
2023-11-24 05:27:28 -0800

    Configuration management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creates a file in the `/tmp` directory
+
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
