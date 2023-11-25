dc4978d22efdc34999d3636b0d4172b764e6a773
unawarexi <unawarexi@gmail.com>
2023-11-24 18:24:13 -0800

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,16 @@
-#!/usr/bin/pup
-# Using Puppt to install flask from pip3
-package {'flask':
+#!/usr/bin/puppet
+
+# Using Puppet to install Flask from pip3
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip',
+  require  => Exec['update_pip'], # Ensure that pip is up-to-date before installing
 }
+
+# Exec resource to update pip3
+exec { 'update_pip':
+  command     => 'pip3 install --upgrade pip',
+  path        => ['/usr/local/bin', '/usr/bin'],
+  refreshonly => true,
+}
+
