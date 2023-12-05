9bdb22c1c4ef48dc03cf82c09efdb2beee667ee0
unawarexi <unawarexi@gmail.com>
2023-12-05 23:51:37 +0100

    Load Balancer

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,16 @@
+# Puppet manifest to configure Nginx with a custom 404 page and a redirection rule.
+
+exec {'update':
+  command => '/usr/bin/apt-get update',
+}
+-> package {'nginx':
+  ensure => 'present',
+}
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
+}
